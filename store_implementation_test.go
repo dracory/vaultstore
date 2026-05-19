8e84b323d194ffd1727ba6b0254ee7ab48972aef
Milan <lesichkovm@gmail.com>
2026-05-19 07:47:25 +0100

    test: update Test_Store_AutoMigrate to pass context.Background() to MigrateUp

--- store_implementation_test.go
+++ store_implementation_test.go
@@ -105,7 +105,7 @@ func Test_Store_AutoMigrate(t *testing.T) {
 		t.Fatalf("automigrateEnabled: Expected [false] received [%v]", store.automigrateEnabled)
 	}
 
-	err = store.MigrateUp()
+	err = store.MigrateUp(context.Background())
 
 	if err != nil {
 		t.Fatalf("AutoMigrate Failure [%v]", err.Error())
